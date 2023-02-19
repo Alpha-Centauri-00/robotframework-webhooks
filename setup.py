fb67afd6c0f61a71ed77afc4df8bea26966fe3c4
Ó_Centauri <56655042+Alpha-Centauri-00@users.noreply.github.com>
2023-02-19 11:31:47 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,39 @@
+import re
+
+from setuptools import setup
+from setuptools import find_packages
+from os.path import abspath, dirname, join
+
+CURDIR = dirname(abspath(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open(
+    join(CURDIR, "webhooks", "__init__.py"), encoding="utf-8"
+) as f:
+    VERSION = re.search('\n__version__ = "(.*)"', f.read()).group(1)
+
+setup(
+    name="robotframework-webhooks",
+    version=VERSION,
+    author="M.Kherki(Alpha-Centauri-00)",
+    author_email="alpha_Centauri@posteo.de",
+    description="A listener that sends a Stack Trace instantly to MS-Tams or Slack where the failure appears.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Alpha-Centauri-00/robotframework-webhooks",
+    package_dir={"": "webhooks"},
+    packages=find_packages("webhooks"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Acceptance",
+        "Framework :: Robot Framework",
+    ],
+    install_requires=["robotframework >= 3.2"],
+    python_requires=">=3.6",
+)
